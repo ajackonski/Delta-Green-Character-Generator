88967adc275d8ccc4449e93d9b6ec9e9a9cff7ff
Alex Jackonski <ajackonski1@gmail.com>
2022-11-28 17:40:19 -0600

    added random statistic generation

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import random
 print("Welcome to the Delta Green Agent Generator!")
+
+def stat_generation():
+    pass
+    stats = ["STR", "DEX", "CON", "INT", "POW", "CHA"]
+    dice_rolls = []
+    for i in stats:
+        current_min = random.randint(1,6)
+        for k in range(1,4):
+            ran_num = random.randint(1,6)
+            if ran_num >= current_min:
+                dice_rolls.append(ran_num)
+            else:
+                dice_rolls.append(current_min)
+                current_min = ran_num
+        final_val = sum(dice_rolls)
+        print(f"{i}: {final_val}")
+        dice_rolls.clear()
+init_ = input("Would you like to generate an Agent? y/n ")
+if init_ == "y":
+    print("Your Agent's stats are:")
+    stat_generation()
+else:
+    quit()

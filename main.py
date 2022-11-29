834327ddd3f4a9135469994274b917de928b17d6
Alex Jackonski <ajackonski1@gmail.com>
2022-11-28 19:59:53 -0600

    moved dice roll functions to the character class

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-import random
+from character import Character
 print("Welcome to the Delta Green Agent Generator!")
-
-def stat_generation():
-    pass
-    stats = ["STR", "DEX", "CON", "INT", "POW", "CHA"]
-    dice_rolls = []
-    for i in stats:
-        current_min = random.randint(1,6)
-        for k in range(1,4):
-            ran_num = random.randint(1,6)
-            if ran_num >= current_min:
-                dice_rolls.append(ran_num)
-            else:
-                dice_rolls.append(current_min)
-                current_min = ran_num
-        final_val = sum(dice_rolls)
-        print(f"{i}: {final_val}")
-        dice_rolls.clear()
-init_ = input("Would you like to generate an Agent? y/n ")
-if init_ == "y":
-    print("Your Agent's stats are:")
-    stat_generation()
-else:
-    quit()
+init_ = input("Would you like to generate an Agent? y/n ").lower()
+if init_ == "y" or init_ == "yes":
+    name = input("What is your Agent's name? ")
+    agent = Character(name)
+    agent.generate_init_stats_()
+    print(agent)

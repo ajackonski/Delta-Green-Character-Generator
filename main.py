24ddf51b01bd807866f7179e15f32d13670c6a41
Alex Jackonski <ajackonski1@gmail.com>
2022-11-29 05:17:41 -0600

    removed y/n prompt

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from character import Character
 print("Welcome to the Delta Green Agent Generator!")
-init_ = input("Would you like to generate an Agent? y/n ").lower()
-if init_ == "y" or init_ == "yes":
-    name = input("What is your Agent's name? ")
-    agent = Character(name)
-    agent.generate_init_stats_()
-    print(agent)
-else:
-    print("Damn that sucks")
+name = input("What is your Agent's name? ")
+agent = Character(name)
+agent.generate_init_stats_()
+print(agent)

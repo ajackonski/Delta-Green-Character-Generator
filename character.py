fe69e77c3219f23221e7373100b89d719a5a2ce3
Alex Jackonski <ajackonski1@gmail.com>
2022-11-29 18:19:43 -0600

    changed some attributes under the character init

--- character.py
+++ character.py
@@ -11,11 +11,9 @@ class Character:
         self.athletics = 30
         self.bureaucracy = 10
         self.computer_science = 0
-        self.craft = 0
-        self.craft_electrician = 0
-        self.craft_mechanic = 0
-        self.craft_microelectronics = 0
-        self.craft_custom = 0
+        self.craft1 = 0
+        self.craft2 = 0
+        self.craft3 = 0
         self.criminology = 10
         self.demolitions = 0
         self.disguise = 10
@@ -34,7 +32,9 @@ class Character:
         self.law = 0
         self.medicine = 0
         self.melee_weapons = 30
-        self.military_science = 0
+        self.military_science1 = 0
+        self.military_science2 = 0
+        self.military_science3 = 0
         self.navigate = 10
         self.occult = 10
         self.persuade = 20
@@ -78,7 +78,8 @@ class Character:
                 current_min = ran_num
         final_val = sum(dice_rolls)
         return final_val
- #add logic to professions that prevent a skill from being set above 80.  
+ #add logic to professions that prevent a skill from being set above 80.
+ #add logic to allow user to pick skills from the pick 2 section of each profession  
     def anthropologist(self):
         self.anthropolgy += 50
         self.bureaucracy += 40
@@ -91,9 +92,9 @@ class Character:
 
     def computer_scientist(self):
         self.computer_science += 60
-        self.craft_electrician += 30
-        self.craft_mechanic += 30
-        self.craft_microelectronics += 40
+        self.craft1 += 30
+        self.craft2 += 30
+        self.craft3+= 40
         self.sigint += 40
         self.bonds += 3
 
@@ -116,10 +117,10 @@ class Character:
 Constitution: {self.constitution} \nIntelligence: {self.intelligence} \nPower: {self.power} \nCharisma: {self.charisma} 
 Max HP: {self.max_hit_points} \nMax Willpower: {self.max_willpower_points} \nCurrent Sanity: {self.starting_sanity_points} \nBreaking Point: {self.breaking_point} \nBonds: {self.bonds} 
 Skills---------------------------- \nAccounting: {self.accounting} \nAlertness: {self.alertness} \nAnthropology: {self.anthropolgy} \nArcheology: {self.archeology}
-Art: {self.art} \nArtillery: {self.artillery} \nAthletics: {self.athletics} \nBureaucracy: {self.bureaucracy} \nComputer Science: {self.computer_science} \nCraft: {self.craft} 
-Criminology: {self.criminology} \nDemolitions: {self.demolitions} \nDisguise: {self.disguise} \nDodge: {self.dodge} \nDrive: {self.drive} \nFirearms: {self.firearms} \nFirst Aid: {self.first_aid}
+Art: {self.art} \nArtillery: {self.artillery} \nAthletics: {self.athletics} \nBureaucracy: {self.bureaucracy} \nComputer Science: {self.computer_science} \nCraft1: {self.craft1} 
+Craft2: {self.craft2} \nCraft3: {self.craft3} \nCriminology: {self.criminology} \nDemolitions: {self.demolitions} \nDisguise: {self.disguise} \nDodge: {self.dodge} \nDrive: {self.drive} \nFirearms: {self.firearms} \nFirst Aid: {self.first_aid}
 Foreign Language 1: {self.foreign_language1} \nForeign Language 2: {self.foreign_language2} \nForeign Language 3: {self.foreign_language3} \nForensics: {self.forensics} \nHeavy Machinery {self.heavy_machinery} 
-Heavy Weapons: {self.heavy_weapons} \nHistory: {self.history} \nHUMINT: {self.humint} \nLaw: {self.law} \nMedicine: {self.medicine} \nMelee Weapons: {self.melee_weapons} \nMilitary Science: {self.military_science}
+Heavy Weapons: {self.heavy_weapons} \nHistory: {self.history} \nHUMINT: {self.humint} \nLaw: {self.law} \nMedicine: {self.medicine} \nMelee Weapons: {self.melee_weapons} \nMilitary Science1: {self.military_science1}
 Navigate: {self.navigate} \nOccult: {self.occult} \nPersuade: {self.persuade} \nPharmacy: {self.pharmacy} \nPilot: {self.pilot} \nPsychotherapy: {self.psychotherapy} \nRide: {self.ride} \nScience: {self.science}
-Search: {self.search} \nSIGINT: {self.sigint} \nStealth: {self.stealth} \nSurgery: {self.surgery} \nSurvival: {self.survival} \nSwim: {self.swim} \nUnarmed Combat: {self.unarmed_combat} \nUnnatural: {self.unnatural}
+Military Science2: {self.military_science2} \nMilitary Science3: {self.military_science3} \nSearch: {self.search} \nSIGINT: {self.sigint} \nStealth: {self.stealth} \nSurgery: {self.surgery} \nSurvival: {self.survival} \nSwim: {self.swim} \nUnarmed Combat: {self.unarmed_combat} \nUnnatural: {self.unnatural}
 """   
